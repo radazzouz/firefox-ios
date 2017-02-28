349a322c7031507efaa13b77ce36546fe844897d
James Hugman <james@hugman.tv>
2017-02-28 14:59:15 +0000

    Swift 3.0 – PushClientTests

--- AccountTests/PushClientTests.swift
+++ AccountTests/PushClientTests.swift
@@ -1,4 +1,3 @@
-//
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
@@ -8,38 +7,25 @@ import Foundation
 import Shared
 import XCTest
 
-private let timeoutPeriod: NSTimeInterval = 600
-
 class PushClientTests: XCTestCase {
 
     var endpointURL: NSURL {
         return DeveloperPushConfiguration().endpointURL
     }
-
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
     
     func testClientRegistration() {
-        let expectation = expectationWithDescription(#function)
         let num = arc4random_uniform(1 << 31)
         let deviceID = "test-id-deadbeef-\(num)"
         let client = PushClient(endpointURL: endpointURL)
 
-        client.register(deviceID) >>== { registration in
-            print("Registered: \(registration.uaid)")
+        let maybeReg = client.register(deviceID).value
+        XCTAssert(maybeReg.isSuccess, "Registered OK - deviceID = \(deviceID)")
 
-            client.unregister(registration).upon { _ in
-                print("Unregistered: \(registration.uaid)")
-                expectation.fulfill()
-            }
+        guard let registration = maybeReg.successValue else {
+            return XCTFail("Registration failed – \(maybeReg.failureValue)")
         }
-        waitForExpectationsWithTimeout(timeoutPeriod, handler: nil)
+
+        let maybeVoid = client.unregister(registration).value
+        XCTAssert(maybeVoid.isSuccess, "Unregistered OK - deviceID = \(deviceID), registration.uaid = \(registration.uaid)")
     }
 }
